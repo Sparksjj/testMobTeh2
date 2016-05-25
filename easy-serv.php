152bfcfc59d1f6da26f1fa0ffc43e61dcd42d63d
Yauhen <Arucard2009@yandex.ru>
2016-05-25 22:50:03 +0300

    add bad localStorege

--- easy-serv.php
+++ easy-serv.php
@@ -1,27 +1,38 @@
 <?php
 $answerJson = file_get_contents('php://input');
 $answer = json_decode($answerJson, true);
+
+if (isset($_GET['email']) && isset($_GET['password']) && $_GET['email']!="undefined" && $_GET['password']!="undefined") {
+
+	$file = 'data/users.json';
+    $users = file_get_contents($file);
+    $users = substr($users, 7, -2);
+    $users_arr = explode("},{", $users);
  
-if(isset($answer['title']) && isset($answer['mess']) && !isset($answer['messId']))
-{
+    foreach ($users_arr as $value) {
+    	if( strripos($value, '"email":"'.$_GET['email'].'"') !==false && strripos($value, '"password":"'.$_GET['password'].'"') !==false){
+			$value = json_encode('{'.$value.'}');
+			echo $value; die();
+    	}
+    }
+    echo ""; die();
+}
+
+if(isset($answer['title']) && isset($answer['mess']) && !isset($answer['messId'])) {
 	$file = 'data/messages.json';
     $messages = file_get_contents($file);
     $sms = ','.$answerJson.']';
-    $messages = substr($messages, 0, -1).$sms;
-    echo $messages;
+    $messages = substr($messages, 0, -1).$sms;    
     file_put_contents($file, $messages);
-    
-    /*echo $answerJson;*/
+    echo $messages; die();
  
-}
-else if(isset($answer['messId']))
-{  
+} else if(isset($answer['messId'])) {  
     $file = 'data/answers.json';
     $answers = file_get_contents($file);
     $sms = ','.$answerJson.']';
-    $answers = substr($answers, 0, -1).$sms;
-    echo $answers;
+    $answers = substr($answers, 0, -1).$sms;    
     file_put_contents($file, $answers);
+    echo $answers; die();
 }
 
 
--- js/app.js
+++ js/app.js
@@ -30,9 +30,9 @@ app.controller('loginCtrl', ['$scope', 'authorizationFactory', '$location',
 function($scope, authorizationFactory, $location){
   
   $scope.loginClick = function($event) {
-    console.log(authorizationFactory.isAdmin());
+
     if (authorizationFactory.login($scope.userEmail, $scope.userPassword)) {
-      console.log("ура");
+      console.log($scope.userEmail);
     } else {
       console.log("о нет"); 
     }
@@ -48,9 +48,12 @@ app.factory('authorizationFactory',['$userProvider', '$http',
   function($userProvider, $http){
 
     var login = function(login, pass){
-      $http.get('/easy-serv.php').success(function(data, status, headers, config){
+      $http.get('/easy-serv.php?email='+login+'&password='+pass).success(function(data, status, headers, config){
+      	
+      	console.log();
 
         if (data != '') {
+          data = JSON.parse(JSON.parse(data));
           if (data.admin) {
             $userProvider.setUser({login: data.name, email: data.email, roles: [$userProvider.rolesEnum.admin]});
           }else{
@@ -60,7 +63,7 @@ app.factory('authorizationFactory',['$userProvider', '$http',
         }else{
           return false;
         }
-
+        
       })
     }
     
@@ -97,11 +100,11 @@ app.factory('$userProvider', function(){
   };
 
   var setUser = function(u){
-    sessionStorage.currentUser = angular.toJson(u);
+    localStorage.currentUser = angular.toJson(u);
   }
 
   var getUser = function(){
-    return sessionStorage.currentUser
+    return localStorage.currentUser
   }
  
   return {
